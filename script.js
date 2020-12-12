f91c8ded90926cd3519a58fe2fa76802677ca0a5
Nipun3120 <nipunverma39@gmail.com>
2020-12-12 21:22:11 +0530

    updated index.html and style.css

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var modal = document.querySelector('.modal-box');
 var button = document.querySelector('.story');
 var close = document.querySelector(".close")[0];
 
+// open modal
 button.addEventListener('click', ()=>{
     modal.style.display = "block";
 })
 
-
+// close modal 
 function closeModal(){
     modal.style.display = "none";
     
